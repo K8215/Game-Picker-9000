452751bb0b545cbf11c9917949408ec700e4fd7f
Kate Coleman <k8lak8@protonmail.com>
2021-08-31 20:42:21 -0400

    quick edit to 404 type size. added playtime for each game. Added checkbox to filter out games youve already played.

--- public/js/game-picker.js
+++ public/js/game-picker.js
@@ -1,76 +1,89 @@
 //Selectors
-const inputId = document.querySelector('#inputId');
-const buttonSubmit = document.querySelector('#buttonSubmit');
-const randContainer = document.querySelector('#randContainer');
-const outputRand = document.querySelector('#outputRand');
-const buttonRand = document.querySelector('#buttonRand');
-const allcontainer = document.querySelector('#allContainer');
-const outputTotal = document.querySelector('#outputTotal');
-const outputList = document.querySelector('#outputList');
-const loader = document.querySelector('#loader');
-const errorMessage = 'Could not retrieve games.';
+const inputId = document.querySelector('#inputId')
+const inputCheckbox = document.querySelector('#inputCheckbox')
+const buttonSubmit = document.querySelector('#buttonSubmit')
+const randContainer = document.querySelector('#randContainer')
+const outputRand = document.querySelector('#outputRand')
+const buttonRand = document.querySelector('#buttonRand')
+const allcontainer = document.querySelector('#allContainer')
+const outputTotal = document.querySelector('#outputTotal')
+const outputList = document.querySelector('#outputList')
+const loader = document.querySelector('#loader')
+const errorMessage = 'Could not retrieve games.'
 
 //Init
 buttonSubmit.addEventListener('click', function (e) {
     e.preventDefault();
 
     //Grab user's input
-    let steamId = inputId.value;
+    let steamId = inputId.value
 
     //Show loader
-    loader.style.display = 'block';
+    loader.style.display = 'block'
 
     //Display games
     getGames(steamId)
         .then((gameList) => {
-            loadGames();
-            generateList(gameList);
-            randomize(gameList);
+            loadGames()
+            generateList(gameList)
+            randomize(gameList)
         })
         .catch((err) => {
-            console.log(err);
-            outputRand.innerHTML = errorMessage;
-            outputList.innerHTML = errorMessage;
+            console.log(err)
+            outputRand.innerHTML = errorMessage
+            outputList.innerHTML = errorMessage
         });
 });
 
 //Functions
 async function getGames(steamId) {
-    const api_url = `/games/${steamId}`;
-    const response = await fetch(api_url);
-    const json = await response.json();
-    const gameList = json.response.games;
+    const api_url = `/games/${steamId}`
+    const response = await fetch(api_url)
+    const json = await response.json()
+    let gameList
 
-    if (response.status !== 200) {
-        throw new Error(errorMessage);
+    //Filter out played games if the box is checked
+    if (inputCheckbox.checked) {
+        const unfiltered = json.response.games
+        gameList = unfiltered.filter(game => !((game.playtime_forever / 60) > 1))
+    } else {
+        gameList = json.response.games
     }
 
-    return gameList;
+    if (response.status !== 200) {
+        throw new Error(errorMessage)
+    }
+
+    return gameList
 };
 
 function generateList(gameList) {
     gameList.forEach(function (game) {
-        var newLi = document.createElement('li');
-        var name = document.createTextNode(`${game.name}`)
+        var newLi = document.createElement('li')
+        var playtime = Math.round(game.playtime_forever / 60)
 
-        newLi.appendChild(name);
+        newLi.innerHTML = `
+        <h4>${game.name}</h4>
+        <span class="game-time">Playtime: ${playtime} hrs</span>
+        `
+
         outputList.appendChild(newLi)
     });
 
-    outputTotal.innerHTML = gameList.length;
+    outputTotal.innerHTML = gameList.length
 }
 
 function randomize(gameList) {
-    var rand = Math.floor(Math.random() * gameList.length);
-    outputRand.innerHTML = gameList[rand].name;
+    var rand = Math.floor(Math.random() * gameList.length)
+    outputRand.innerHTML = gameList[rand].name
 
     buttonRand.addEventListener('click', () => {
-        randomize(gameList);
+        randomize(gameList)
     });
 }
 
 function loadGames() {
-    loader.style.display = 'none';
-    randContainer.style.display = 'block';
-    allContainer.style.display = 'block';
+    loader.style.display = 'none'
+    randContainer.style.display = 'block'
+    allContainer.style.display = 'block'
 }
